e974355145b7b4ba860b3b90f8d4a714dca1dc6a
m-okeefe <megan037@gmail.com>
2018-10-25 10:18:02 -0700

    Init

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+)
+
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	message := "Hello World v1"
+	w.Write([]byte(message))
+}
+func main() {
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for sig := range c {
+			fmt.Printf("Signal received: %v", sig)
+			os.Exit(0)
+		}
+	}()
+
+	http.HandleFunc("/", sayHello)
+	fmt.Println("Hello world server listening on 8080...")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		panic(err)
+	}
+}
