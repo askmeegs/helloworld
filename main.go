7e07964d89dca8dd6524444dee8b7c4576d511f8
m-okeefe <megan037@gmail.com>
2018-10-29 09:36:06 -0700

    Saving work

--- main.go
+++ main.go
@@ -5,12 +5,25 @@ import (
 	"net/http"
 	"os"
 	"os/signal"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
 	message := "Hello World v1"
+	opsProcessed.Inc()
 	w.Write([]byte(message))
 }
+
+var (
+	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "megan_helloworld_processed_ops_total",
+		Help: "The total number of processed hello calls",
+	})
+)
+
 func main() {
 
 	c := make(chan os.Signal, 1)
@@ -22,6 +35,7 @@ func main() {
 		}
 	}()
 
+	http.Handle("/metrics", promhttp.Handler())
 	http.HandleFunc("/", sayHello)
 	fmt.Println("some log message")
 	fmt.Println("Hello world server listening on 8080...")
